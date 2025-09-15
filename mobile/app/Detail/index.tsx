5fcfb45650050fd24592c838cba5218258b69aee
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2025-09-15 18:14:00 -0300

    Detail page

--- mobile/app/Detail/index.tsx
+++ mobile/app/Detail/index.tsx
@@ -1,10 +1,122 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Constants from 'expo-constants'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from 'expo-router';
+import { RectButton } from 'react-native-gesture-handler'
+
 
 const Detail = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack()
+  }
+
   return (
-    <View />
+    <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+        <Image style={styles.pointImage} source={{ uri: 'https://plus.unsplash.com/premium_photo-1663099654523-d3862b7742cd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }} />
+        <Text style={styles.pointName}>Lixão</Text>
+        <Text style={styles.pointItems}>pilhas...</Text>
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endreco</Text>
+          <Text style={styles.addressContent}>Japaratinga, AL</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => { }}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    paddingBottom: 46,
+  },
+
+  pointImage: {
+    width: '100%',
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    marginTop: 32,
+  },
+
+  pointName: {
+    color: '#322153',
+    fontSize: 28,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  pointItems: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  address: {
+    marginTop: 32,
+  },
+
+  addressTitle: {
+    color: '#322153',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+
+  addressContent: {
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  footer: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: '#999',
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    paddingBottom: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  button: {
+    width: '48%',
+    backgroundColor: '#34CB79',
+    borderRadius: 10,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    marginLeft: 8,
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Roboto_500Medium',
+  },
+});
 
 export default Detail
